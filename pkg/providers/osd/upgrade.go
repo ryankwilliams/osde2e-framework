474f87e3f9f705e8d36ad2c5ed8416f86ce2d5ac
Ryan Williams <3375653+ryankwilliams@users.noreply.github.com>
2023-05-24 09:18:59 -0400

    Revise ocm upgrade method after initial commit

--- pkg/providers/osd/upgrade.go
+++ pkg/providers/osd/upgrade.go
@@ -2,6 +2,7 @@ package osd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,8 +18,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/utils/pointer"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
 	"sigs.k8s.io/e2e-framework/klient/wait"
 	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 )
@@ -28,6 +30,7 @@ const (
 	managedUpgradeOperatorNamespace      = "openshift-managed-upgrade-operator"
 	versionGateLabel                     = "api.openshift.com/gate-ocp"
 	upgradeMaxAttempts                   = 1080
+	upgradeDelay                         = 10
 )
 
 // upgradeError represents the cluster upgrade custom error
@@ -35,7 +38,7 @@ type upgradeError struct {
 	err error
 }
 
-// Error returns the formatted error message when upgradeError is invokved
+// Error returns the formatted error message when upgradeError is invoked
 func (e *upgradeError) Error() string {
 	return fmt.Sprintf("osd upgrade failed: %v", e.err)
 }
@@ -44,7 +47,7 @@ func (e *upgradeError) Error() string {
 func (o *Provider) versionGates(ctx context.Context) (*clustersmgmtv1.VersionGateList, error) {
 	response, err := o.ClustersMgmt().V1().VersionGates().List().SendContext(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("unable to list version gates: %v", err)
+		return nil, fmt.Errorf("failed to get version gates: %v", err)
 	}
 
 	return response.Items(), nil
@@ -54,7 +57,7 @@ func (o *Provider) versionGates(ctx context.Context) (*clustersmgmtv1.VersionGat
 func (o *Provider) getVersionGateID(ctx context.Context, version string) (string, error) {
 	versionGates, err := o.versionGates(ctx)
 	if err != nil {
-		return "", fmt.Errorf("unable to get version gate id for version %q: %v", version, err)
+		return "", fmt.Errorf("failed to get version gate id for version %q, %v", version, err)
 	}
 
 	for _, versionGate := range versionGates.Slice() {
@@ -63,14 +66,14 @@ func (o *Provider) getVersionGateID(ctx context.Context, version string) (string
 		}
 	}
 
-	return "", fmt.Errorf("version gate does not exist for version %q", version)
+	return "", fmt.Errorf("no version gate exists for %q", version)
 }
 
-// getGateAgreement returns the gate agreement ocm resource
-func (o *Provider) getGateAgreement(ctx context.Context, versionGateID string) (*clustersmgmtv1.VersionGate, error) {
+// getVersionGateAgreement returns the gate agreement ocm resource
+func (o *Provider) getVersionGateAgreement(ctx context.Context, versionGateID string) (*clustersmgmtv1.VersionGate, error) {
 	response, err := o.ClustersMgmt().V1().VersionGates().VersionGate(versionGateID).Get().SendContext(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("unable to get version gate agreement id %q: %v", versionGateID, err)
+		return nil, fmt.Errorf("failed  to get version gate agreement %q, %v", versionGateID, err)
 	}
 
 	return response.Body(), nil
@@ -80,7 +83,7 @@ func (o *Provider) getGateAgreement(ctx context.Context, versionGateID string) (
 func (o *Provider) gateAgreementExistForCluster(ctx context.Context, clusterID, gateAgreementID string) (bool, error) {
 	response, err := o.ClustersMgmt().V1().Clusters().Cluster(clusterID).GateAgreements().List().SendContext(ctx)
 	if err != nil {
-		return false, fmt.Errorf("unable to get cluster id %q version gate agreements: %v", clusterID, err)
+		return false, fmt.Errorf("failed to get cluster %q version gate agreement: %v", clusterID, err)
 	}
 
 	for _, gateAgreement := range response.Items().Slice() {
@@ -116,7 +119,7 @@ func (o *Provider) addGateAgreement(ctx context.Context, clusterID string, curre
 		return nil
 	}
 
-	gateAgreement, err := o.getGateAgreement(ctx, versionGateID)
+	gateAgreement, err := o.getVersionGateAgreement(ctx, versionGateID)
 	if err != nil {
 		return err
 	}
@@ -125,12 +128,12 @@ func (o *Provider) addGateAgreement(ctx context.Context, clusterID string, curre
 		VersionGate(clustersmgmtv1.NewVersionGate().Copy(gateAgreement)).
 		Build()
 	if err != nil {
-		return fmt.Errorf("building version gate agreement for cluster id %q failed: %v", clusterID, err)
+		return fmt.Errorf("failed to build version gate agreement for cluster %q, %v", clusterID, err)
 	}
 
 	_, err = o.ClustersMgmt().V1().Clusters().Cluster(clusterID).GateAgreements().Add().Body(versionGateAgreement).SendContext(ctx)
 	if err != nil {
-		return fmt.Errorf("applying version gate agreement to cluster id %q failed: %v", clusterID, err)
+		return fmt.Errorf("failed to apply version gate agreement to cluster %q, %v", clusterID, err)
 	}
 
 	return nil
@@ -142,12 +145,12 @@ func (o *Provider) initiateUpgrade(ctx context.Context, clusterID, version strin
 		NextRun(time.Now().UTC().Add(7 * time.Minute)).
 		ScheduleType("manual").Build()
 	if err != nil {
-		return fmt.Errorf("unable to build upgrade policy for cluster id %q: %v", clusterID, err)
+		return fmt.Errorf("failed to build upgrade policy for cluster %q, %v", clusterID, err)
 	}
 
 	response, err := o.ClustersMgmt().V1().Clusters().Cluster(clusterID).UpgradePolicies().Add().Body(upgradePolicy).SendContext(ctx)
 	if err != nil || response.Status() != http.StatusCreated {
-		return fmt.Errorf("applying upgrade policy to cluster id %q failed: %v", clusterID, err)
+		return fmt.Errorf("failed to apply upgrade policy to cluster %q, %v", clusterID, err)
 	}
 
 	log.Printf("Cluster id %q upgrade to version %q has been scheduled for %s\n", clusterID, response.Body().Version(), response.Body().NextRun().Format(time.RFC3339))
@@ -157,23 +160,46 @@ func (o *Provider) initiateUpgrade(ctx context.Context, clusterID, version strin
 
 // restartManagedUpgradeOperator scales down/up the muo operator to speed up the cluster upgrade start time
 func (o *Provider) restartManagedUpgradeOperator(ctx context.Context, client *kubernetes.Client) error {
-	// TODO fix this
+	patchReplicas := func(replicasCount int) (*k8s.Patch, error) {
+		patchData, err := json.Marshal(map[string]interface{}{
+			"spec": map[string]interface{}{
+				"replicas": replicasCount,
+			},
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to build patch data to modify deployment %s replicas count: %v", managedUpgradeOperatorDeploymentName, err)
+		}
+
+		return &k8s.Patch{
+			PatchType: types.StrategicMergePatchType,
+			Data:      patchData,
+		}, nil
+	}
+
 	deployment := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: managedUpgradeOperatorDeploymentName, Namespace: managedUpgradeOperatorNamespace}}
 	err := wait.For(conditions.New(client.Resources).DeploymentConditionMatch(deployment, appsv1.DeploymentAvailable, corev1.ConditionTrue))
 	if err != nil {
-		return fmt.Errorf("unable to get managed upgrade operator deployment: %v", err)
+		return fmt.Errorf("failed to get managed upgrade operator deployment: %v", err)
 	}
 
-	deployment.Spec.Replicas = pointer.Int32(0)
-	err = client.Update(ctx, deployment)
+	patchData, err := patchReplicas(0)
 	if err != nil {
-		return fmt.Errorf("scale down %s deployment replicas to 0 failed: %v", managedUpgradeOperatorDeploymentName, err)
+		return err
 	}
 
-	deployment.Spec.Replicas = pointer.Int32(1)
-	err = client.Update(ctx, deployment)
+	err = client.Patch(ctx, deployment, *patchData)
 	if err != nil {
-		return fmt.Errorf("scale up %s deployment replicas to 1 failed: %v", managedUpgradeOperatorDeploymentName, err)
+		return fmt.Errorf("failed to scale down %s deployment: %v", managedUpgradeOperatorDeploymentName, err)
+	}
+
+	patchData, err = patchReplicas(1)
+	if err != nil {
+		return err
+	}
+
+	err = client.Patch(ctx, deployment, *patchData)
+	if err != nil {
+		return fmt.Errorf("failed to scale up %s deployment: %v", managedUpgradeOperatorDeploymentName, err)
 	}
 
 	log.Println("Successfully restarted managed upgrade operator")
@@ -192,109 +218,114 @@ func (o *Provider) managedUpgradeConfigExist(ctx context.Context, dynamicClient 
 		return nil
 	}
 
-	return fmt.Errorf("managed upgrade config never existed on the cluster")
+	return fmt.Errorf("managed upgrade config does not exist the cluster")
 }
 
-// TODO Come back and revise/retest, initial first draft/port
 // OCMUpgrade handles the end to end process to upgrade an openshift dedicated cluster
 func (o *Provider) OCMUpgrade(ctx context.Context, client *kubernetes.Client, clusterID string, currentVersion, upgradeVersion semver.Version) error {
-	dynamicClient, err := getKubernetesDynamicClient(client)
-	if err != nil {
+	var (
+		conditionMessage string
+		dynamicClient    *dynamic.DynamicClient
+		err              error
+		upgradeStatus    string
+	)
+
+	if dynamicClient, err = getKubernetesDynamicClient(client); err != nil {
 		return &upgradeError{err: err}
 	}
 
-	err = o.addGateAgreement(ctx, clusterID, currentVersion, upgradeVersion)
-	if err != nil {
+	if err = o.restartManagedUpgradeOperator(ctx, client); err != nil {
 		return &upgradeError{err: err}
 	}
 
-	err = o.initiateUpgrade(ctx, clusterID, upgradeVersion.String())
-	if err != nil {
+	if err = o.addGateAgreement(ctx, clusterID, currentVersion, upgradeVersion); err != nil {
 		return &upgradeError{err: err}
 	}
 
-	err = o.restartManagedUpgradeOperator(ctx, client)
-	if err != nil {
+	if err = o.initiateUpgrade(ctx, clusterID, upgradeVersion.String()); err != nil {
 		return &upgradeError{err: err}
 	}
 
-	err = o.managedUpgradeConfigExist(ctx, dynamicClient)
-	if err != nil {
+	if err = o.restartManagedUpgradeOperator(ctx, client); err != nil {
 		return &upgradeError{err: err}
+	}
+
+	if err = o.managedUpgradeConfigExist(ctx, dynamicClient); err != nil {
+		return &upgradeError{err: err}
+	}
+
+	errorHandler := func(key string, found bool, err error) error {
+		if !found || err != nil {
+			log.Printf("Failed to retrieve %q from managed upgrade operator config: %v\n", key, err)
+			time.Sleep(10 * time.Second)
+			return err
+		}
+		return nil
 	}
 
 	for i := 1; i <= upgradeMaxAttempts; i++ {
-		var upgradeStatus string
-		var conditionMessage string
-
 		upgradeConfig, err := getManagedUpgradeOperatorConfig(ctx, dynamicClient)
 		if err != nil {
-			time.Sleep(10 * time.Second)
+			log.Printf("Failed to get managed upgrade operator config: %v\n", err)
+			time.Sleep(upgradeDelay * time.Second)
 			continue
 		}
 
 		status, found, err := unstructured.NestedMap(upgradeConfig.Object, "status")
-		if !found || err != nil {
-			time.Sleep(10 * time.Second)
+		if errorHandler("status", found, err) != nil {
 			continue
 		}
 
 		histories, found, err := unstructured.NestedSlice(status, "history")
-		if !found || err != nil {
-			time.Sleep(10 * time.Second)
+		if errorHandler("status.history", found, err) != nil {
 			continue
 		}
 
 		for _, h := range histories {
 			version, found, err := unstructured.NestedString(h.(map[string]interface{}), "version")
-			if !found || err != nil {
-				time.Sleep(10 * time.Second)
+			if errorHandler("status.history.[].version", found, err) != nil {
 				continue
 			}
 
 			if version == upgradeVersion.String() {
 				upgradeStatus, found, err = unstructured.NestedString(h.(map[string]interface{}), "phase")
-				if !found || err != nil {
-					time.Sleep(10 * time.Second)
+				if errorHandler("status.history.[].version.phase", found, err) != nil {
 					continue
 				}
 
 				conditions, found, err := unstructured.NestedSlice(h.(map[string]interface{}), "conditions")
-				if !found || err != nil {
-					time.Sleep(10 * time.Second)
+				if errorHandler("status.history.[].version.conditions", found, err) != nil {
 					continue
 				}
 
 				conditionMessage, found, err = unstructured.NestedString(conditions[0].(map[string]interface{}), "message")
-				if !found || err != nil {
-					time.Sleep(10 * time.Second)
+				if errorHandler("status.history.[].version.message", found, err) != nil {
 					continue
 				}
 
 				break
 			}
-			_ = h
 		}
 
-		if upgradeStatus == "" {
-			log.Println("Upgrade has not started yet")
-			time.Sleep(10 * time.Second)
-			continue
-		}
-
-		if upgradeStatus == "Failed" {
-			log.Printf("Upgrade %q, %s", upgradeStatus, conditionMessage)
-			return fmt.Errorf("upgrade failed")
-		}
-
-		if upgradeStatus != "Upgraded" {
-			log.Printf("Upgrade is %q, %s\n", upgradeStatus, conditionMessage)
-			time.Sleep(10 * time.Second)
-			continue
+		switch upgradeStatus {
+		case "":
+			log.Println("Upgrade has not start yet..")
+			time.Sleep(upgradeDelay * time.Second)
+		case "Failed":
+			log.Printf("Upgrade failed, %s\n", conditionMessage)
+			return &upgradeError{err: fmt.Errorf("upgrade failed")}
+		case "Upgraded":
+			log.Printf("Upgrade complete!")
+			return nil
+		case "Pending":
+			log.Printf("Upgrade is pending")
+			time.Sleep(upgradeDelay * time.Second)
+		case "Upgrading":
+			log.Printf("Upgrade is in progress, %s\n", conditionMessage)
 		}
 	}
 
-	return nil
+	return fmt.Errorf("upgrade is still in progress, failed to finish within max wait attempts")
 }
 
 // getKubernetesDynamicClient returns the kubernetes dynamic client
